5b8a448c5ec64b4386bf3c806c2a63f4b43a94dd
Devayani Reddy <devayani03@gmail.com>
2026-06-19 14:22:16 +0530

    add new commit

---
+++ frontend/javascript-practice.js
@@ -0,0 +1,46 @@
+console.log("welcome students");
+var a=10
+var b=20
+console.log(a+b)
+
+let name = "Devayani";
+console.log(name);
+name = "Student";
+console.log(name);
+
+
+var a = 10;
+var a = 20;   // Redeclaration 
+console.log(a);
+
+
+
+//table
+let n = 12;
+
+for(let i = 1; i <= 10; i++) {
+   console.log(n * i);
+}
+
+const age=12
+if (age>=12){
+    console.log("eligible to vote")
+}
+else{
+    console.log("")
+}
+
+
+let a = 10;
+let b = 25;
+let c = 18;
+
+if (a > b && a > c) {
+    console.log(a + " is greater");
+}
+else if (b > a && b > c) {
+    console.log(b + " is greater");
+}
+else {
+    console.log(c + " is greater");
+}
